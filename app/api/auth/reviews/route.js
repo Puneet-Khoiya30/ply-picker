cbe4c4bf1e543023d887abf832139bc0a32e64cb
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2024-09-02 23:53:12 +0530

    dd

--- app/api/auth/reviews/route.js
+++ app/api/auth/reviews/route.js
@@ -1,11 +1,11 @@
-import connectToDatabase from '@/lib/mongodb';
-import Review from '@/models/Review';
-import { NextResponse } from 'next/server';
+// import connectToDatabase from '@/lib/mongodb';
+// import Review from '@/models/Review';
+// import { NextResponse } from 'next/server';
 
-export async function POST(request) {
-  await connectToDatabase();
-  const data = await request.json();
-  const review = new Review(data);
-  await review.save();
-  return NextResponse.json(review);
-}
+// export async function POST(request) {
+//   await connectToDatabase();
+//   const data = await request.json();
+//   const review = new Review(data);
+//   await review.save();
+//   return NextResponse.json(review);
+// }
